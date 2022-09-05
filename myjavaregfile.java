34135a7b6150d3cbefc7b1484b70d7037a43e744
hari <hari@gmail.com>
2022-09-05 15:25:37 +0530

    mng work

--- myjavaregfile.java
+++ myjavaregfile.java
@@ -1,2 +1,9 @@
 this is my java file
+<<<<<<< Updated upstream
 my manager work
+=======
+1st day of java file
+2nd day of java file
+3rd day of java file
+4th day of java file
+>>>>>>> Stashed changes
